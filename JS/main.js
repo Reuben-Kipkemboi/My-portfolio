20e8b3369edc42ca60268ac7d9f7327f7cf56160
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2023-04-08 21:28:09 +0300

    st:paragraph-styling - optimize paragraph styling

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,12 @@
+var typed = new Typed(".typing", {
+    strings: ["A Full Stack software Engineer...", "UI/UX Enthusiast...",
+        "Creating web solutions..."
+    ],
+    typeSpeed: 80,
+    backSpeed: 80,
+    loop: true
+})
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
