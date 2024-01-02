b936e3283e98324285650cf9e08b81511112f753
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2024-01-02 12:17:49 +0300

    st:paragraph-styling  - optimize paragraph styling

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
 var typed = new Typed(".typing", {
     strings: ["A Full Stack software Engineer...", "UI/UX Enthusiast...",
-        "Creating web solutions..."
+        "Creating 🔥 web solutions ..."
     ],
     typeSpeed: 80,
     backSpeed: 80,
@@ -10,7 +10,7 @@ var typed = new Typed(".typing", {
 $(document).ready(function() {
     setTimeout(function() {
       $('.navbar').addClass('animate__animated animate__backInDown'); // Add the animation classes after the delay
-    }, 4); // Delay in milliseconds (adjust the value as needed)
+    }, 5); // Delay in milliseconds 
   });
 
 
